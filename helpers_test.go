bc1bb49ec459712b70e0ef32394e1de2a3557b7f
Arnaud V <vaz-git@gmx.com>
2015-08-20 23:09:48 +0200

    Add benchmark for the DiffSlices function

--- helpers_test.go
+++ helpers_test.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	// "fmt"
 	"strings"
 	"testing"
 )
@@ -8,6 +9,11 @@ import (
 const (
 	text     = "A collection of helper functions about anything and everything"
 	textSHA1 = "037b993cade2a9493c8ac2f9766c6ec5889311f5"
+)
+
+var (
+	sliceIntX = []int{46, 77, 89, 43, 3, 33, 0, 10, 95, 83, 16, 1, 9, 82, 48, 36, 2, 59, 45, 12}
+	sliceIntY = []int{5, 93, 54, 10, 48, 66, 97, 46, 11, 8, 64, 37, 39, 89, 71, 40, 19, 63, 87, 65}
 )
 
 // --- --- --- Tests
@@ -66,10 +72,18 @@ func Benchmark_StringInSlice(b *testing.B) {
 	b.StopTimer()
 }
 
-// Benchmark for the StringInSlice function
+// Benchmark for the SHA1hash function
 func Benchmark_SHA1hash(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		SHA1hash(text)
+	}
+	b.StopTimer()
+}
+
+// Benchmark for the DiffSlices function
+func Benchmark_DiffSlices(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		DiffSlices(sliceIntX, sliceIntY)
 	}
 	b.StopTimer()
 }
