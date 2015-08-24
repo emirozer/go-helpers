0b78aba16f3fd6c076a574d34231a85db6e6307f
Emir Ozer <emirozer@yandex.com>
2015-08-24 10:11:26 +0200

    added new func local ipv4

--- helpers.go
+++ helpers.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	rnd "math/rand"
+	"net"
 	"os"
 	"reflect"
 	"strings"
@@ -143,4 +144,16 @@ func UpperCaseFirst(s string) string {
 	phrase := []rune(s)
 	phrase[0] = unicode.ToUpper(phrase[0])
 	return string(phrase)
+}
+
+// GetLocalIpv4 returns a string of the host machine local ipv4
+func GetLocalIpv4() string {
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			return fmt.Sprintf("%s", ipv4)
+		}
+	}
+	return "localhost"
 }
